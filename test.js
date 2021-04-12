a40a57a31df24cae3a2f36d56a6e42289b248660
ThePianoSG <yongmeng@thepiano.sg>
2021-04-12 14:30:07 +0800

    deleted package lock

--- test.js
+++ test.js
@@ -4,3 +4,13 @@ if (debugThrowError) {
 	throw new Error();
 	//
 }
+
+/*
+ffmpeg -protocol_whitelist "file,http,https,tcp,tls" -i index2.m3u8 -c copy output.mkv
+
+
+
+
+src="https://cdnapisec.kaltura.com/p/2082311/sp/208231100/playManifest/protocol/https/entryId/1_rqmmv3sn/format/applehttp/tags/web_hd/f/a.m3u8?clientTag=html5:v1.2.1&playSessionId=0984e17a-c200-720c-8468-fa23493260b0:23acb760-ce27-14c3-a5ff-66373bd9162a"
+
+*/
